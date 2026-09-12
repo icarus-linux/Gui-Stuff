bd611886d7020841ed8236bbb83446bd893c3cb2
icarus <aldenesterhuizen1@gmail.com>
2026-09-12 21:03:36 +0200

    added some context from help

--- gui_calculator.py
+++ gui_calculator.py
@@ -1,3 +1,13 @@
+#Function	Description
+#tk.Tk()	Initializes the main window.
+#title("Window Title")	Sets the title of the window.
+#configure(background)	Changes the background color of the window.
+#geometry("widthxheight")	Sets the size of the window.
+#mainloop()	Starts the Tkinter event loop to display the window.
+
+
+
+
 import tkinter as tk
 
 
